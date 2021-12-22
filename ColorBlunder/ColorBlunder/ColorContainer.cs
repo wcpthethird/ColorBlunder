629569cc366a4e1445ee8d195d02b96e7b81d041
Will Petersen <10553340@uvu.edu>
2021-12-22 12:07:22 -0700

    Add project files.

---
+++ ColorBlunder/ColorBlunder/ColorContainer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace ColorBlunder
+{
+    public class ColorContainer
+    {
+        readonly Random rand = new Random();
+        public List<Color> solutionColors;
+        public List<Color> shuffledColors;
+
+        public ColorContainer()
+        {
+            ValidateColors();
+        }
+
+        public void ValidateColors()
+        {
+            List<Color> baseColors = new List<Color>()
+            {
+                RandomColor(),
+                RandomColor()
+            };
+
+            float colorDifference = Math.Abs(baseColors[1].GetHue() - baseColors[0].GetHue());
+
+            if (CheckValue(colorDifference))
+            {
+                GenerateColorGradient(baseColors, 10);
+            }
+            else
+            {
+                ValidateColors();
+            }
+        }
+
+        public void GenerateColorGradient(List<Color> baseColors, int size)
+        {
+            var tempGradientList = new List<Color>();
+            for (int i = 0; i < size; i++)
+            {
+                var rAverage = baseColors[0].R + (baseColors[1].R - baseColors[0].R) * i / size;
+                var gAverage = baseColors[0].G + (baseColors[1].G - baseColors[0].G) * i / size;
+                var bAverage = baseColors[0].B + (baseColors[1].B - baseColors[0].B) * i / size;
+                tempGradientList.Add(Color.FromArgb(rAverage, gAverage, bAverage));
+            }
+            solutionColors = tempGradientList
+                .OrderBy(color => color.GetHue())
+                .ToList();
+
+            shuffledColors = solutionColors
+                .GetRange(1, 9)
+                .OrderBy(color => rand.Next())
+                .ToList();
+        }
+
+        bool CheckValue(double difference)
+        {
+            return (difference >= 45 && difference <= 90);
+        }
+
+        public Color RandomColor()
+        {
+            int r = rand.Next(255);
+            int g = rand.Next(255);
+            int b = rand.Next(255);
+            Color startColor = Color.FromArgb(r, g, b);
+            return startColor;
+        }
+    }
+}
---
+++ ColorBlunder/ColorBlunder/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Color = System.Drawing.Color;
+
+namespace ColorBlunder
+{
+    public partial class MainPage : ContentPage
+    {
+        List<Color> shuffledColors;
+        private List<Color> _solutionColors;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadColors();
+        }
+
+        public void LoadColors()
+        {
+            ColorContainer hueContainer = new ColorContainer();
+
+            _solutionColors = hueContainer.solutionColors;
+            shuffledColors = hueContainer.shuffledColors;
+            SetSolutionColors();
+        }
+
+        private void SetSolutionColors()
+        {
+            Box0_0.BackgroundColor = _solutionColors[0];
+            Box0_1.BackgroundColor = _solutionColors[1];
+            Box0_2.BackgroundColor = _solutionColors[2];
+            Box0_3.BackgroundColor = _solutionColors[3];
+            Box0_4.BackgroundColor = _solutionColors[4];
+            Box0_5.BackgroundColor = _solutionColors[5];
+            Box0_6.BackgroundColor = _solutionColors[6];
+            Box0_7.BackgroundColor = _solutionColors[7];
+            Box0_8.BackgroundColor = _solutionColors[8];
+            Box0_9.BackgroundColor = _solutionColors[9];
+        }
+
+        public void SetShuffledColors()
+        {
+            Box0_1.BackgroundColor = shuffledColors[0];
+            Box0_2.BackgroundColor = shuffledColors[1];
+            Box0_3.BackgroundColor = shuffledColors[2];
+            Box0_4.BackgroundColor = shuffledColors[3];
+            Box0_5.BackgroundColor = shuffledColors[4];
+            Box0_6.BackgroundColor = shuffledColors[5];
+            Box0_7.BackgroundColor = shuffledColors[6];
+            Box0_8.BackgroundColor = shuffledColors[7];
+        }
+
+        private void BtnGenerate_Clicked(object sender, EventArgs e)
+        {
+            LoadColors();
+        }
+
+        private void BtnPlay_Clicked(object sender, EventArgs e)
+        {
+            SetShuffledColors();
+        }
+    }
+}
